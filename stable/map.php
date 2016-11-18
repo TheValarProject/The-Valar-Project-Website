ff971bde3e9a171870a5db54337a3d4c8a297a63
scribblemaniac <scribblemaniac@users.noreply.github.com>
2016-11-17 18:19:46 -0700

    Add fullscreen support to map
    Closes #25

--- stable/map.php
+++ stable/map.php
@@ -5,17 +5,55 @@ include('pageContent.php');
 <html>
  <head>
   <title>Map - The Valar Project</title>
+  <?php head(); ?>
   <style>
+#map-wrapper {
+ border-color: #e8ac3e #e8ac3e #39290a #39290a;
+ border-style: solid;
+ border-width: 2px;
+}
+
 #map {
- width: 950px;
+ width: 949px;
  height: 400px;
+ border-color: #39290a #39290a #e8ac3e #e8ac3e;
+ border-style: solid;
+ border-width: 2px;
+}
+
+.button-wrapper {
+  float: right;
+}
+
+.button-wrapper,.button-wrapper > input {
+  border-top: none;
 }
   </style>
-  <?php head(); ?>
+  <script type="text/javascript">
+$(document).ready(function() {
+  var map = $("#map")[0];
+  $("#fullscreen").click(function() {
+    if(map.requestFullscreen) {
+      map.requestFullscreen();
+    } else if(map.mozRequestFullScreen) {
+      map.mozRequestFullScreen();
+    } else if(map.webkitRequestFullscreen) {
+      map.webkitRequestFullscreen();
+    } else if(map.msRequestFullscreen) {
+      map.msRequestFullscreen();
+    } else {
+      window.location = $(map).attr("src");
+    }
+  });
+});
+  </script>
  </head>
  <body>
   <?php bodyStart(); ?>
-  <iframe src="http://<?php echo gethostbyname('server.tvp.elementfx.com') == $_SERVER['REMOTE_ADDR'] ? 'localhost' : "server.tvp.elementfx.com"; ?>:8123" id="map"></iframe>
+  <div id="map-wrapper">
+    <iframe src="http://<?php echo gethostbyname('server.tvp.elementfx.com') == $_SERVER['REMOTE_ADDR'] ? 'localhost' : "server.tvp.elementfx.com"; ?>:8123" id="map" allowfullscreen></iframe>
+  </div>
+  <div class="button-wrapper"><input type="button" value="Fullscreen" id="fullscreen" /></div>
   <?php bodyEnd(); ?>
  </body>
 </html>
