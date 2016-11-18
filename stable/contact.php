1a5a4d263dc172e8e757b7534848b505ba2613bc
scribblemaniac <scribblemaniac@users.noreply.github.com>
2016-11-17 23:09:51 -0700

    Add contact contents

--- stable/contact.php
+++ stable/contact.php
@@ -9,7 +9,12 @@ include('pageContent.php');
  </head>
  <body>
   <?php bodyStart(); ?>
-This website is still under construction and this page is not available. Please be patient while we work on getting everything online. Thank you for your patience.
+  <h1>Contact Us</h1>
+  <p>There are many ways to contact us. You can of course talk to us on the server, but if we're not around, you can send us an email at: <a href="mailto:contact@tvp.elementfx.com">contact@tvp.elementfx.com</a>. We should get back to you within a few days. You can email us your Skype username and we can add you to our Skype group. If Google+ is your thing you can find us <a href="https://plus.google.com/108717602686570585467">here</a>.</p>
+  <h2>Server-Specific Contacts</h2>
+  <p>To contact us about the server, you can use are primary email mentioned above. You can also post comments on <a href="http://www.planetminecraft.com/server/the-valar-project-1839157/">our Planet Minecraft page</a>. We also have a <a href="https://www.facebook.com/pages/The-Valar-Project-MC/514615731895565">Facebook page</a> for the server, however it is currently abandoned (we are hoping to get it back up soon).</p>
+  <h2>Mod-Specific Contacts</h2>
+  <p>To contact us about the mod, you can use are primary email mentioned above. You can also post comments on <a href="http://www.planetminecraft.com/mod/awaken-dreams-the-valar-project/">our Planet Minecraft page</a>. If you find bugs, you can report them in our <a href="https://github.com/TheValarProject/AwakenDreamsClient">GitHub repository</a>. We also have a <a href="https://www.facebook.com/AwakenDreamsMod">Facebook page</a> for the mod, however it is currently abandoned (we are hoping to get it back up soon).</p>
   <?php bodyEnd(); ?>
  </body>
 </html>
