41e2ea67adb47819d3c7919d4daf1548ddb78786
scribblemaniac <scribblemaniac@users.noreply.github.com>
2016-11-18 00:46:44 -0700

    Add mod content

--- stable/mod.php
+++ stable/mod.php
@@ -9,7 +9,10 @@ include('pageContent.php');
  </head>
  <body>
   <?php bodyStart(); ?>
-This website is still under construction and this page is not available. Please be patient while we work on getting everything online. Thank you for your patience.
+  <h1>The Awaken Dreams Mod</h1>
+  <p>The Awaken Dreams mod is a major part of The Valar Project and it aims to enhance Minecraft to provide a better experience on our server, and in the future will also work to create a unique adventure experience on single-player mode as well. It currently adds hundreds of custom blocks and items to help make the whole experience more authentic.</p>
+  <p>The mod was programmed entriely by the current project leader, scribblemaniac. Because of this, development has been very slow. The mod has now been made open source, and is available for anyone to contribute to on <a href="https://github.com/TheValarProject/AwakenDreamsClient">Github</a>. Hopefully our openness will enourage new developers and result in more frequent releases. Even if this does not happen, we will still make progress slowly and steadly so please be patient.</p>
+  <p>We have a lot in store for the mod, so please check out our <a href="/modPlans.php">plans</a> for more details.</p>
   <?php bodyEnd(); ?>
  </body>
 </html>
--- stable/modPlans.php
+++ stable/modPlans.php
@@ -13,7 +13,7 @@ include('pageContent.php');
   <?php bodyStart(); ?>
   <h1>v0.5</h1>
   <div>
-   <p>Our current goal is to finish v0.5. This version will add many more blocks and items as usually. The primary focus is on bug fixes, upgrading to the latest minecraft, and integration with <a href="http://cuberite.org/">Cuberite</a> for the server. Some roleplaying features or mods may also get put in here.</p>
+   <p>Our current goal is to finish v0.5. This version will add many more blocks and items as usual. The primary focus is on bug fixes, upgrading to the latest minecraft, and integration with <a href="http://cuberite.org/">Cuberite</a> for the server. Some roleplaying features or mods may also get put in here.</p>
   </div>
   <br />
   <p>An <span style="font-style: italic;">extremely</span> detailed plan for the mod is currently in the works. When it is finished it will be added here. It will aim to cover every tiny detail of the mod and will hopefully give guidance to the future of the mod.</p>
