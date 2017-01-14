332c4ffdfa60720ff939b0c0b825a63d5d122247
scribblemaniac <scribblemaniac@users.noreply.github.com>
2017-01-14 13:51:00 -0700

    Add link to our Discord server and remove Skype group info

--- stable/community.php
+++ stable/community.php
@@ -10,7 +10,7 @@ include('pageContent.php');
  <body>
   <?php bodyStart(); ?>
   <h1>Community</h1>
-  <p>Currently you can join our community by actively participating on the server. If you email us (see <a href="/contact.php">Contact Us</a>) with your Skype username we can add you to our Skype group. We have a <a href="https://www.youtube.com/channel/UCDR5oPhtfihRyX9oEi3on2g">Youtube channel</a> where we post updates, features, tutorials, and more so subscribe to that so you don't miss out. We also have a <a href="https://plus.google.com/108717602686570585467?pageId=115475084700439800717">Googe+ account</a> and a <a href="https://www.facebook.com/pages/The-Valar-Project-MC/514615731895565">Facebook page</a>, but these aren't very active right now.</p>
+  <p>Currently you can join our community by actively participating on the server. Our primary form of instant communication is on our <a href="https://discord.gg/hvJrwYX">Discord server</a>. We have a <a href="https://www.youtube.com/channel/UCDR5oPhtfihRyX9oEi3on2g">Youtube channel</a> where we post updates, features, tutorials, and more so subscribe to that so you don't miss out. We also have a <a href="https://plus.google.com/108717602686570585467?pageId=115475084700439800717">Googe+ account</a> and a <a href="https://www.facebook.com/pages/The-Valar-Project-MC/514615731895565">Facebook page</a>, but these aren't very active right now.</p>
   <p>We are planning to do additional things to promote a community among our users, including forums, a messenging system, and a wiki. However we have not finished these projects yet, so please be patient while we work on them.</p>
   <?php bodyEnd(); ?>
  </body>
